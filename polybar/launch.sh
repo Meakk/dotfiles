177923ed427944c9bbe158c8db00326575fe6c4b
Michael Migliore <michael.migliore@kitware.com>
2019-01-14 18:49:19 +0100

    List monitors

--- polybar/launch.sh
+++ polybar/launch.sh
@@ -1,9 +1,10 @@
 #!/bin/env sh
 
 pkill polybar
+sleep 1
 
-sleep 1;
-
-MONITOR=eDP-1 polybar topbar &
-MONITOR=DP-1-2 polybar topbar &
-MONITOR=DP-1-3 polybar topbar &
+lines=$(polybar -m | sed -e 's/:.*$//g')
+for mon in $lines
+do
+  MONITOR=$mon polybar topbar &
+done
