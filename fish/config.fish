04e97f8a3e2ddaf79ad90416706b93aafaae730a
Michael Migliore <michael.migliore@kitware.com>
2019-01-15 18:00:02 +0100

    adjust ui

--- fish/config.fish
+++ fish/config.fish
@@ -1,3 +1,7 @@
 set -x QT_QPA_PLATFORMTHEME qt5ct
 set -x QT_LOGGING_RULES "qt5ct.debug=false"
 set -x TERMINAL kitty
+set -x fish_color_command 8BE9FD --bold
+set -x fish_color_quote F1FA8C
+set -x fish_color_redirection FF79C6
+set -x fish_color_end F1FA8C
