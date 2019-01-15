3a2643f957e55929a1a739ec82f9503ea26b6783
Michael Migliore <michael.migliore@kitware.com>
2019-01-15 09:46:57 +0100

    Fix wlan

--- polybar/launch.sh
+++ polybar/launch.sh
@@ -3,7 +3,7 @@
 pkill polybar
 
 eth=$(ip link show | grep enp | sed -E "s/.*(enp.*):.*/\1/g" | head -n 1)
-wlan=$(ip link show | grep enp | sed -E "s/.*(wlp.*):.*/\1/g" | head -n 1)
+wlan=$(ip link show | grep wlp | sed -E "s/.*(wlp.*):.*/\1/g" | head -n 1)
 
 lines=$(polybar -m | sed -e 's/:.*$//g')
 for mon in $lines
