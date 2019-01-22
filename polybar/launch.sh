b9995c078452246ecc3ea9c0e28a5d60e936cc01
Michael Migliore <michael.migliore@kitware.com>
2019-01-22 18:01:16 +0100

    some tweaks

--- polybar/launch.sh
+++ polybar/launch.sh
@@ -4,9 +4,10 @@ pkill polybar
 
 eth=$(ip link show | grep enp | sed -E "s/.*(enp.*):.*/\1/g" | head -n 1)
 wlan=$(ip link show | grep wlp | sed -E "s/.*(wlp.*):.*/\1/g" | head -n 1)
+vpn=$(ip link show | grep tun | sed -E "s/.*(tun.*):.*/\1/g" | head -n 1)
 
 lines=$(polybar -m | sed -e 's/:.*$//g')
 for mon in $lines
 do
-  CHECKUPDATES_DB=$(mktemp -d) WLAN=$wlan ETH=$eth MONITOR=$mon polybar topbar &
+  CHECKUPDATES_DB=$(mktemp -d) WLAN=$wlan ETH=$eth VPN=$vpn MONITOR=$mon polybar topbar &
 done
--- polybar/vpn.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-echo $(ip address show dev tun0 2> /dev/null | grep inet | awk '{print $2}' | cut -f2 -d ':')
-
