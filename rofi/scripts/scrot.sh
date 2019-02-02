53685e02cdcf97c4a64cf961162821f036189657
Michael Migliore <mcmigliore@gmail.com>
2019-02-02 20:47:05 +0100

    changes icons

--- rofi/scripts/scrot.sh
+++ rofi/scripts/scrot.sh
@@ -9,11 +9,11 @@ export SCROTUR="$(xrdb -query | grep "*theme_ug" | cut -f 2)"
 
 rofi_command="rofi -theme ~/.cache/wal/scrot.rasi"
 # Each of the icon is a selectable element
-options=$'\n\n'
+options=$'\n\n'
 
 chosen="$(echo "$options" | $rofi_command -dmenu)"
 case $chosen in
-    ) # Scrot a rectangular area
+    ) # Scrot a rectangular area
         i3-scrot -s
         ;;
     ) # Scrot the whole screen
