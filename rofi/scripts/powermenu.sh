4265479842666682195e79ae993d187dea3662fa
Michael Migliore <michael.migliore@kitware.com>
2019-02-27 17:09:04 +0100

    rofi, trying to compute padding

--- rofi/scripts/powermenu.sh
+++ rofi/scripts/powermenu.sh
@@ -2,14 +2,14 @@
 rofi_command="rofi -theme ~/.cache/wal/powermenu.rasi"
 
 # Each of the icon is a selectable element
-options=$'\n\n痢\nﮤ'
+options=$'\n\n痢\n襤'
 
 chosen="$(echo "$options" | $rofi_command -dmenu)"
 case $chosen in
     ) # Lock the screen
         sleep 0.3; sh $HOME/.cache/wal/lock.sh
         ;;
-    ﮤ) # Shutdown the computer
+    襤) # Shutdown the computer
         poweroff
         ;;
     痢) # Reboot the computer
--- rofi/scripts/scrot.sh
+++ rofi/scripts/scrot.sh
@@ -11,7 +11,14 @@ rofi_command="rofi -theme ~/.cache/wal/scrot.rasi"
 # Each of the icon is a selectable element
 options=$'\n\nﬓ'
 
-chosen="$(echo "$options" | $rofi_command -dmenu)"
+# padding depending on resolution
+resolution=$(xrandr | grep $(bspc query -m -M --names) | sed -r 's/(.*) ([[:digit:]]+x[[:digit:]]+)(.*)/\2/')
+
+padding="$(expr $(echo $resolution | cut -d 'x' -f 1) / 2 - 320)px $(expr $(echo $resolution | cut -d 'x' -f 2) / 2 - 100)px"
+
+echo $padding
+
+chosen="$(echo "$options" | $rofi_command -dmenu -theme-str 'window { padding: "$hpadding" "$wpadding"; }')"
 case $chosen in
     ) # Scrot a rectangular area
         i3-scrot -s
