6b5df95a4d70039c0ff8476611641ec922403bd8
Michael Migliore <michael.migliore@kitware.com>
2019-03-04 13:22:17 +0100

    fix vivaldi

--- wal/templates/vivaldi.js
+++ wal/templates/vivaldi.js
@@ -1,47 +1,47 @@
 let colors = {{}};
 
-colors.Bg = '{background.rgba}'.split(',');
+colors.Bg = '{background.rgb}'.split(',').concat(1.0);
 colors.BgAlpha = '{background.rgb}'.split(',').concat(0.8);
 colors.BgAlphaHeavy = '{background.rgb}'.split(',').concat(0.6);
 colors.BgAlphaHeavier = '{background.rgb}'.split(',').concat(0.2);
-colors.BgDark = '{background.rgba}'.split(',');
-colors.BgDarker = '{background.rgba}'.split(',');
-colors.BgLight = '{background.rgba}'.split(',');
-colors.BgLighter = '{background.rgba}'.split(',');
-colors.BgLightIntense = '{background.rgba}'.split(',');
-colors.BgIntense = '{background.rgba}'.split(',');
-colors.BgIntenser = '{background.rgba}'.split(',');
-colors.BgInverse = '{background.rgba}'.split(',');
-colors.BgInverser = '{background.rgba}'.split(',');
-colors.BgFaded = '{background.rgba}'.split(',');
-colors.Fg = '{foreground.rgba}'.split(',');
+colors.BgDark = '{background.rgb}'.split(',').concat(1.0);
+colors.BgDarker = '{background.rgb}'.split(',').concat(1.0);
+colors.BgLight = '{background.rgb}'.split(',').concat(1.0);
+colors.BgLighter = '{background.rgb}'.split(',').concat(1.0);
+colors.BgLightIntense = '{background.rgb}'.split(',').concat(1.0);
+colors.BgIntense = '{background.rgb}'.split(',').concat(1.0);
+colors.BgIntenser = '{background.rgb}'.split(',').concat(1.0);
+colors.BgInverse = '{background.rgb}'.split(',').concat(1.0);
+colors.BgInverser = '{background.rgb}'.split(',').concat(1.0);
+colors.BgFaded = '{background.rgb}'.split(',').concat(1.0);
+colors.Fg = '{foreground.rgb}'.split(',').concat(1.0);
 colors.FgAlpha = '{foreground.rgb}'.split(',').concat(0.1);
-colors.FgIntense = '{foreground.rgba}'.split(',');
-colors.FgFaded = '{foreground.rgba}'.split(',');
-colors.FgFadedMore = '{foreground.rgba}'.split(',');
-colors.HighlightBg = '{color9.rgba}'.split(',');
+colors.FgIntense = '{foreground.rgb}'.split(',').concat(1.0);
+colors.FgFaded = '{foreground.rgb}'.split(',').concat(1.0);
+colors.FgFadedMore = '{foreground.rgb}'.split(',').concat(1.0);
+colors.HighlightBg = '{color9.rgb}'.split(',').concat(1.0);
 colors.HighlightBgAlpha = '{color9.rgb}'.split(',').concat(0.25);
-colors.HighlightBgDark = '{color1.rgba}'.split(',');
-colors.HighlightFg = '{foreground.rgba}'.split(',');
+colors.HighlightBgDark = '{color1.rgb}'.split(',').concat(1.0);
+colors.HighlightFg = '{foreground.rgb}'.split(',').concat(1.0);
 colors.HighlightFgAlpha = '{foreground.rgb}'.split(',').concat(0.5);
 colors.HighlightFgAlphaHeavy = '{foreground.rgb}'.split(',').concat(0.25);
-colors.AccentBg = '{color4.rgba}'.split(',');
+colors.AccentBg = '{color4.rgb}'.split(',').concat(1.0);
 colors.AccentBgAlpha = '{color4.rgb}'.split(',').concat(0.4);
 colors.AccentBgAlphaHeavy = '{color4.rgb}'.split(',').concat(0.25);
-colors.AccentBgDark = '{color4.rgba}'.split(',');
-colors.AccentBgDarker = '{color4.rgba}'.split(',');
-colors.AccentBgFaded = '{color12.rgba}'.split(',');
-colors.AccentBgFadedMore = '{color12.rgba}'.split(',');
-colors.AccentBgFadedMost = '{color12.rgba}'.split(',');
-colors.AccentBorder = '{color13.rgba}'.split(',');
-colors.AccentBorderDark = '{color5.rgba}'.split(',');
-colors.AccentFg = '{foreground.rgba}'.split(',');
-colors.AccentFgFaded = '{foreground.rgba}'.split(',');
+colors.AccentBgDark = '{color4.rgb}'.split(',').concat(1.0);
+colors.AccentBgDarker = '{color4.rgb}'.split(',').concat(1.0);
+colors.AccentBgFaded = '{color12.rgb}'.split(',').concat(1.0);
+colors.AccentBgFadedMore = '{color12.rgb}'.split(',').concat(1.0);
+colors.AccentBgFadedMost = '{color12.rgb}'.split(',').concat(1.0);
+colors.AccentBorder = '{color13.rgb}'.split(',').concat(1.0);
+colors.AccentBorderDark = '{color5.rgb}'.split(',').concat(1.0);
+colors.AccentFg = '{foreground.rgb}'.split(',').concat(1.0);
+colors.AccentFgFaded = '{foreground.rgb}'.split(',').concat(1.0);
 colors.AccentFgAlpha = '{foreground.rgb}'.split(',').concat(0.25);
-colors.Border = '{color0.rgba}'.split(',');
-colors.BorderHighlight = '{color0.rgba}'.split(',');
-colors.BorderDisabled = '{color0.rgba}'.split(',');
-colors.BorderIntense = '{background.rgba}'.split(',');
+colors.Border = '{color0.rgb}'.split(',').concat(1.0);
+colors.BorderHighlight = '{color0.rgb}'.split(',').concat(1.0);
+colors.BorderDisabled = '{color0.rgb}'.split(',').concat(1.0);
+colors.BorderIntense = '{background.rgb}'.split(',').concat(1.0);
 
 function inject() {{
   // remove header
@@ -55,7 +55,7 @@ function inject() {{
 
   // override css vars
   for (c in colors)
-    app.style.setProperty('--color' + c, 'rgba(' + colors[c].join(',') + ') !important');
+    app.style.setProperty('--color' + c, 'rgb(' + colors[c].join(',') + ')');
 }};
 
 // wait for browser to load
