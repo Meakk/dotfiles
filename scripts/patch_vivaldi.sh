e38d31b160abe55b9d4563c9f5a399563f997d0c
Michael Migliore <michael.migliore@kitware.com>
2019-03-07 18:12:43 +0100

    tweaks

--- scripts/patch_vivaldi.sh
+++ scripts/patch_vivaldi.sh
@@ -20,18 +20,6 @@ fi
 # backup browser.html
 cp "$vivaldi_path/resources/vivaldi/browser.html" "$vivaldi_path/resources/vivaldi/browser.html-$(date +%Y-%m-%dT%H-%M-%S)"
 
-# add custom.css
-if [ -f "$patch_dir/vivaldi.css" ] ; then
-    # copy css
-    cp -f "$patch_dir/vivaldi.css" "$vivaldi_path/resources/vivaldi/style/custom.css"
-
-    # patch browser.html
-    css_patched=$(grep '<link rel="stylesheet" href="style/custom.css" />' $vivaldi_path/resources/vivaldi/browser.html)
-    if [ "$css_patched" = "" ] ; then
-        sed -i -e 's/<\/head>/<link rel="stylesheet" href="style\/custom.css" \/> <\/head>/' "$vivaldi_path/resources/vivaldi/browser.html"
-    fi
-fi
-
 # add custom.js
 if [ -f "$patch_dir/vivaldi.js" ] ; then
     # copy js
