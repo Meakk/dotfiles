250a9b5988d1765a1f2b45467c2281c44bab564b
Michael Migliore <mcmigliore@gmail.com>
2019-03-08 21:44:26 +0100

    fixes, cleanup

--- fish/functions/fish_greeting.fish
+++ fish/functions/fish_greeting.fish
@@ -1,9 +1,18 @@
 function fish_greeting
-  ufetch
   for c in (set_color -c | head -n 8)
-    echo -n -s (string repeat -n 4 " ")
-    echo -n -s (set_color $c) "●" (set_color br$c) "●"
+    set -a dots (echo -n -s (set_color $c) ● (set_color br$c) ● "  ")
   end
-  echo
+  set -l highlg (set_color -o yellow)
+  set -l accent (set_color -o blue)
+  set -l normal (set_color -o normal)
+  
+  echo -s $dots[1] $spacing $highlg $USER "@" (hostname)
+  echo -s $dots[2] $spacing $accent "OS:       " $normal "Arch Linux"
+  echo -s $dots[3] $spacing $accent "KERNEL:   " $normal (uname -sr)
+  echo -s $dots[4] $spacing $accent "UPTIME:   " $normal (uptime -p | sed 's/up //')
+  echo -s $dots[5] $spacing $accent "PACKAGES: " $normal (pacman -Q | wc -l)
+  echo -s $dots[6] $spacing $accent "TERMINAL: " $normal $TERMINAL
+  echo -s $dots[7] $spacing $accent "SHELL:    " $normal (basename $SHELL)
+  echo -s $dots[8] $spacing $accent "WM:       " $normal (tail -n 1 "$HOME/.xinitrc" | cut -d ' ' -f 2)
 end
 
