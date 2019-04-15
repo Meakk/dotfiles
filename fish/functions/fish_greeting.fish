fb862556633cfc3ee4a0444a7c03e8601011cbf2
Michael Migliore <michael.migliore@kitware.com>
2019-04-15 09:38:51 +0200

    small fixes

--- fish/functions/fish_greeting.fish
+++ fish/functions/fish_greeting.fish
@@ -11,14 +11,14 @@ function fish_greeting
     set wm (xprop -id (xprop -root -notype _NET_SUPPORTING_WM_CHECK | cut -d# -f2 | xargs) -notype -len 100 -f _NET_WM_NAME 8t | grep _NET_WM_NAME | cut -d= -f2 | xargs)
   end
 
-  echo -s $dots[1] $spacing $highlg $USER "@" (hostname)
-  echo -s $dots[2] $spacing $accent "OS:       " $normal (cat /etc/os-release | grep PRETTY_NAME | cut -d= -f2 | xargs)
-  echo -s $dots[3] $spacing $accent "KERNEL:   " $normal (uname -sr)
-  echo -s $dots[4] $spacing $accent "UPTIME:   " $normal (uptime -p | sed 's/up //')
-  echo -s $dots[5] $spacing $accent "PACKAGES: " $normal (pacman -Q | wc -l)
-  echo -s $dots[6] $spacing $accent "TERMINAL: " $normal $TERMINAL
-  echo -s $dots[7] $spacing $accent "SHELL:    " $normal (basename $SHELL)
-  echo -n -s $dots[8] $spacing $accent
+  echo -s $dots[1] $highlg $USER "@" (hostname)
+  echo -s $dots[2] $accent "OS:       " $normal (cat /etc/os-release | grep PRETTY_NAME | cut -d= -f2 | xargs)
+  echo -s $dots[3] $accent "KERNEL:   " $normal (uname -sr)
+  echo -s $dots[4] $accent "UPTIME:   " $normal (uptime -p | sed 's/up //')
+  echo -s $dots[5] $accent "PACKAGES: " $normal (pacman -Q | wc -l)
+  echo -s $dots[6] $accent "TERMINAL: " $normal $TERMINAL
+  echo -s $dots[7] $accent "SHELL:    " $normal (basename $SHELL)
+  echo -n -s $dots[8] $accent
   if test -n "$DISPLAY"
     echo -s "WM:       " $normal (xprop -id (xprop -root -notype _NET_SUPPORTING_WM_CHECK | cut -d# -f2 | xargs) -notype -len 100 -f _NET_WM_NAME 8t | grep _NET_WM_NAME | cut -d= -f2 | xargs)
   else
