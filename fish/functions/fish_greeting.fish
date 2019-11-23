549b53df4c046a81628d8ca02b629003fb8322d9
Michael Migliore <mcmigliore@gmail.Com>
2019-11-23 20:24:33 +0100

    Fix OSX greeting

--- fish/functions/fish_greeting.fish
+++ fish/functions/fish_greeting.fish
@@ -6,16 +6,21 @@ function fish_greeting
   set -l accent (set_color -o blue)
   set -l normal (set_color -o normal)
 
-  set wm "no X running"
-  if test -n "$DISPLAY"
-    set wm (xprop -id (xprop -root -notype _NET_SUPPORTING_WM_CHECK | cut -d# -f2 | xargs) -notype -len 100 -f _NET_WM_NAME 8t | grep _NET_WM_NAME | cut -d= -f2 | xargs)
-  end
+  if test (uname) = "Darwin"
+    set -g os "MacOSX"
+    set -g packages (brew list -1 | wc -l | sed 's/^ *//')
+    set -g uptime (uptime | sed 's/up //' | sed 's/, load.*//')
+  else
+    set -g os (cat /etc/os-release | grep PRETTY_NAME | cut -d= -f2 | xargs)
+    set -g packages (pacman -Q | wc -l)
+    set -g uptime (uptime -p | sed 's/up //')
+  end 
 
   echo -s $dots[1] $highlg $USER "@" (hostname)
-  echo -s $dots[2] $accent "OS:       " $normal (cat /etc/os-release | grep PRETTY_NAME | cut -d= -f2 | xargs)
+  echo -s $dots[2] $accent "OS:       " $normal $os
   echo -s $dots[3] $accent "KERNEL:   " $normal (uname -sr)
-  echo -s $dots[4] $accent "UPTIME:   " $normal (uptime -p | sed 's/up //')
-  echo -s $dots[5] $accent "PACKAGES: " $normal (pacman -Q | wc -l)
+  echo -s $dots[4] $accent "UPTIME:   " $normal $uptime
+  echo -s $dots[5] $accent "PACKAGES: " $normal $packages
   echo -s $dots[6] $accent "TERMINAL: " $normal $TERMINAL
   echo -s $dots[7] $accent "SHELL:    " $normal (basename $SHELL)
   echo -n -s $dots[8] $accent
